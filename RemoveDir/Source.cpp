63a418d9aa29f15e021d533c621d99261f629b86
cs226500aa <cs226500aa@gmail.com>
2022-08-27 21:33:03 +0800

    update

--- RemoveDir/Source.cpp
+++ RemoveDir/Source.cpp
@@ -2,6 +2,9 @@
 #include <tchar.h> 
 #include <strsafe.h>
 
+#ifdef DEBUG
+#include <locale>
+#endif // DEBUG
 
 void DeleteDir(TCHAR* dir);
 void DeleteDir(TCHAR* dir, unsigned int deleteParent);
@@ -13,6 +16,9 @@ int _tmain(int argc, TCHAR* argv[])
         _tprintf(TEXT("\nUsage: %s <folder>\n"), argv[0]);
         return 1;
     }
+#ifdef DEBUG
+    setlocale(LC_CTYPE, "");
+#endif // DEBUG
 
     DeleteDir(argv[1]);
 
@@ -24,11 +30,11 @@ void DeleteDir(TCHAR* dir)
     DeleteDir(dir, 1);
 }
 
-void DeleteDir(TCHAR* dir, unsigned int deleteParent)
+void DeleteDir(TCHAR* dir, unsigned int DeleteRootPath)
 {
-    TCHAR szDir[MAX_PATH]{ 0 };
+    TCHAR szDir[MAX_PATH]{ 0 }; 
     StringCchCopy(szDir, MAX_PATH, dir);
-    StringCchCat(szDir, MAX_PATH, L"//*.*");
+    StringCchCat(szDir, MAX_PATH, L"\\*.*"); //EX: dir\*.*
 
     WIN32_FIND_DATAW wFindFileData;
     HANDLE hFind = FindFirstFileW(szDir, &wFindFileData);
@@ -42,24 +48,27 @@ void DeleteDir(TCHAR* dir, unsigned int deleteParent)
             continue;
 
         TCHAR szFile[MAX_PATH];
-        StringCchCopy(szFile, MAX_PATH, dir);
-        StringCchCat(szFile, MAX_PATH, L"//");
-        StringCchCat(szFile, MAX_PATH, wFindFileData.cFileName);
+        StringCchCopy(szFile, MAX_PATH, dir);//EX: dir\*.*
+        StringCchCat(szFile, MAX_PATH, L"\\");//EX: dir\*.*\ 
+        StringCchCat(szFile, MAX_PATH, wFindFileData.cFileName);//EX: dir\*.*\file
 
-        if (wFindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) //如果找到的是目錄，則進入目錄進行遞迴
+        if (wFindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) //如果找到的是目錄
         {
-            DeleteDir(szFile, ++deleteParent);
-            RemoveDirectoryW(szFile);
-            deleteParent--;
+            DeleteDir(szFile, ++DeleteRootPath); //進入目錄進行遞迴
+            RemoveDirectoryW(szFile);//資料夾內檔案清除後移除目錄
+            DeleteRootPath--;
         }
         else //找到的是檔案
         {
-            DeleteFileW(szFile);
+            DeleteFileW(szFile);//清除
         }
     } while (FindNextFileW(hFind, &wFindFileData));
-    if (deleteParent > 0)
+
+    //移除目標根目錄
+    if (DeleteRootPath > 0)
     {
         RemoveDirectoryW(dir);
     }
-    FindClose(hFind);
+
+    FindClose(hFind);//關閉檔案句柄
 }
